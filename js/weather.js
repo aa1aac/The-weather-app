3f7b54555813d9bb46df1e02be00c025ec91299c
aa11ac <ach.aayush.111@gmail.com>
2018-03-02 20:36:28 +0545

    add function

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,26 @@
 if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-        var posn="lat="+position.coords.latitude + "&lon=" + position.coords.longitude;
-        }
-
-}
+                var lat=position.coords.latitude;
+                var lon=position.coords.longitude;
+            
+               
+                var apiUrl="https://fcc-weather-api.glitch.me/api/current?lat="+lat+"&lon="+lon;
+                console.log(apiUrl);
+                    $.get(apiUrl)
+                    .done(function(data){
+                        $(".temperature").html(data.main.temp+"&deg;");
+                        $(".humidity").text(data.main.humidity);
+                        $(".windspeed").text(data.wind.speed);
+                        $(".location").text(data.name);
+                        $(".climate").html('<img src='+'"'+data.weather[0].icon+'">')
+    
+                    })
+                    .fail(function(){
+                        alert("Opps some error occured")
+                
+                    })
+                
+        });      
+ };
+ 
+ 
